a1f84586d7aab9d66bdbd61681b7e0c313731260
Hunor Kovacs <hunor.kovacs@ft.com>
2016-11-24 19:18:52 +0200

    prepare for testing runjob

--- publish_service_test.go
+++ publish_service_test.go
@@ -171,9 +171,6 @@ func TestDeleteJob(t *testing.T) {
 		}
 		var mockQueueSer queueServiceI = mockedQueueService{}
 		var mockHttpSer httpServiceI = mockedHttpService{}
-		if err != nil {
-			t.Fatalf("unexpected error. %v", err)
-		}
 		pubService := publishService{
 			clusterRouterAddress: clusterUrl,
 			queueServiceI: &mockQueueSer,
@@ -197,6 +194,56 @@ func TestDeleteJob(t *testing.T) {
 		if len(pubService.jobs) != test.nJobsAfter {
 			t.Fatalf("wrong number of jobs. diff got vs want:\n%d\n%d", len(pubService.jobs), test.nJobsAfter)
 		}
+	}
+}
+
+func TestRunJob(t *testing.T) {
+	tests := []struct {
+		baseURL    string
+	}{
+		{
+			"http://ip-172-24-158-162.eu-west-1.compute.internal:8080/__topics-transformer/transformers/topics",
+		},
+	}
+	for _, test := range tests {
+		clusterUrl, err := url.Parse("http://ip-172-24-158-162.eu-west-1.compute.internal:8080")
+		if err != nil {
+			t.Fatal(err)
+		}
+		var mockQueueSer queueServiceI = mockedQueueService{}
+		var mockHttpSer httpServiceI = mockedHttpService{}
+		if err != nil {
+			t.Fatalf("unexpected error. %v", err)
+		}
+		baseUrl, err := url.Parse(test.baseURL)
+		if err != nil {
+			t.Fatalf("unexpected error. %v", err)
+		}
+		oneJob := &job{
+			JobID: "job_1",
+			URL: *baseUrl,
+			ConceptType: "topics",
+			IDToTID: map[string]string{},
+			Throttle: 1,
+			Status: defined,
+			FailedIDs: []string{},
+		}
+
+		pubService := publishService{
+			clusterRouterAddress: clusterUrl,
+			queueServiceI: &mockQueueSer,
+			mutex: &sync.RWMutex{},
+			jobs: map[string]*job{
+				"job_1": oneJob,
+			},
+			httpService: &mockHttpSer,
+		}
+
+		pubService.runJob(oneJob, "Basic 1234")
+
+		//if len(pubService.jobs) != test.nJobsAfter {
+		//	t.Fatalf("wrong number of jobs. diff got vs want:\n%d\n%d", len(pubService.jobs), test.nJobsAfter)
+		//}
 	}
 }
 
